d35ec9b390c1fb898b98d161d0af94b599745777
MayaTakenOutOfCode <mayawastaken12@gmail.com>
2024-11-14 10:07:52 +0100

    Task: Prisma handlers for db register

--- prisma/migrations/20241114081044_add_is_admin_field/migration.sql
+++ prisma/migrations/20241114081044_add_is_admin_field/migration.sql
@@ -1,9 +1,1 @@
--- CreateTable
-CREATE TABLE "Users" (
-    "id" SERIAL NOT NULL,
-    "email" TEXT NOT NULL,
-    "password" TEXT NOT NULL,
-    "isAdmin" BOOLEAN NOT NULL DEFAULT false,
 
-    CONSTRAINT "Users_pkey" PRIMARY KEY ("id")
-);
